06d91b432121eecce3fba5f3cd5ef1c8b957848d
Cinthia Sanchez <cinthiasanchez@Cinthias-MacBook-Pro.local>
2023-03-18 23:41:04 -0700

    Card Rendering

--- src/components/CARDS/List.js
+++ src/components/CARDS/List.js
@@ -1,31 +1,58 @@
-export const List = () =>{
-    return(
-      <div className="card m-5" style={{width: "18rem"}}>
-      <img
-        src="https://mdbcdn.b-cdn.net/img/new/standard/city/062.webp"
-        className="card-img-top"
-        alt="Chicago Skyscrapers"
-      />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-      </div>
-      <ul className="list-group list-group-light list-group-small">
-        <li className="list-group-item px-4">Cras justo odio</li>
-        <li className="list-group-item px-4">Dapibus ac facilisis in</li>
-        <li className="list-group-item px-4">Vestibulum at eros</li>
-      </ul>
-      <div className="card-body">
-       
-      </div>
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { endpoints } from "../ENDPOINTS/endpoints";
+
+export const List = ({ characterList }) => {
+  const [characterIdsName, setCharacterIdsName] = useState();
+  const [characterLists, setCharacterLists] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const characters = await axios.get(endpoints.getCharacter);
+        console.log(characters.data.results);
+        setCharacterLists(characters.data.results);
+      } catch (error) {
+        alert(error);
+      }
+    };
+    fetch();
+  }, []);
+  const onCharacterClick = (characterId) => {
+    /*console.log(characterId)*/
+    setCharacterIdsName(characterId);
+  };
+  console.log(characterIdsName);
+  return (
+    <div style={{display:'flex',
+                flexWrap:'wrap',
+                justifyContent:'center'}} className='ListCharacter'>
+      {characterLists.map((character) => {
+        return (
+          <div className="card m-5" style={{ width: "12rem" }}>
+            <img
+              src={character.image}
+              className="card-img-top"
+              alt="Chicago Skyscrapers"
+            />
+            <div className="card-body">
+              <h5
+                className="card-title"
+                onClick={() => onCharacterClick(character.id)}
+                key={character.name}
+              >
+                {character.name}
+              </h5>
+            </div>
+            <ul className="list-group list-group-light list-group-small">
+              <li className="list-group-item px-4">
+                Status:<b style={{ color: "red" }}>{character.status}</b>{" "}
+              </li>
+            </ul>
+          </div>
+        );
+      })}
+      {/* */}
     </div>
-        
-   
-    
-    //2da card
-    //3ra card
-    )
-}
+  );
+};
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,79 +1,58 @@
 import axios from "axios";
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Cards } from "./CARDS/Cards";
 import { endpoints } from "./ENDPOINTS/endpoints";
-/*import {List} from './CARDS/List'*/
+import { List } from "./CARDS/List";
 
 export const Navbar = () => {
-const [characterList, setCharacterList] = useState([])
-const [characterIdsName,setCharacterIdsName ] = useState()
-useEffect(()=>{
-
-
-    
-  const fetch=async()=>{
-    try {
-      const characters = await axios.get(endpoints.getCharacter)
-      console.log(characters.data.results)
-      setCharacterList(characters.data.results);
-    } catch (error) {
-      alert(error)
-    }
-  }
-  fetch()
-},[]) 
-const onCharacterClick = (characterId) =>{
- /*console.log(characterId)*/
- setCharacterIdsName(characterId);
-}
-console.log(characterIdsName)
+  const [characterList, setCharacterList] = useState([]);
+  // const [characterIdsName,setCharacterIdsName ] = useState()
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const characters = await axios.get(endpoints.getCharacter);
+        console.log(characters.data.results);
+        setCharacterList(characters.data.results);
+      } catch (error) {
+        alert(error);
+      }
+    };
+    fetch();
+  }, []);
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark">
-  <div className="container-fluid">
-    <form className="d-flex input-group w-auto">
-      <input
-        type="search"
-        className="form-control rounded"
-        placeholder="Search"
-        aria-label="Search"
-        aria-describedby="search-addon"
-      />
-      <span className="input-group-text text-white border-0" id="search-addon">
-        <i className="fas fa-search"></i>
-      </span>
-    </form>
-  </div>
-</nav>
-<div className="container-fluid">
-  <div className="row">
-    <div className="col-4">
-        <h1>PERSONAJE</h1> 
-        {characterIdsName}
-        <Cards/>
+        <div className="container-fluid">
+          <form className="d-flex input-group w-auto">
+            <input
+              type="search"
+              className="form-control rounded"
+              placeholder="Search"
+              aria-label="Search"
+              aria-describedby="search-addon"
+            />
+            <span
+              className="input-group-text text-white border-0"
+              id="search-addon"
+            >
+              <i className="fas fa-search"></i>
+            </span>
+          </form>
+        </div>
+      </nav>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-4">
+            <h1>PERSONAJE</h1>
+            {/* {characterIdsName} */}
+            <Cards />
+          </div>
+          <div className="col-8" >
+            <h1>CONTENIDO DE PERSONAJES </h1>
+            <List characterList={characterList} />
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="col-8">
-    <h1>CONTENIDO DE PERSONAJES</h1> 
-   
-    {/* <List/> */}
-     { characterList.map((character)=>{
-        return(
-            <p onClick={()=> onCharacterClick(character.id)} key={character.name}>{character.name}</p>
-            
-        )
-     })
-
-     }
-    
-    </div>
-
-  </div>
-</div>
-    </div>
-    
   );
 };
-
-/*export default Navbar; */
-
-
