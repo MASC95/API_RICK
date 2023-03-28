0287e863389dfe7b9b1d017e684aaed9a623887b
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-03-28 00:25:32 -0600

    falta mejorar

--- src/components/CARDS/Cards.js
+++ src/components/CARDS/Cards.js
@@ -1,26 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { getCharacters } from "./api/rickAndMortyApi";
+
 export const Cards = () => {
-  return (
-    <div className="card m-5 " style={{width: "18rem"}}>
-      <img
-        src="https://mdbcdn.b-cdn.net/img/new/standard/city/062.webp"
-        className="card-img-top"
-        alt="Chicago Skyscrapers"
-      />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    getCharacters().then(characters => setCharacters(characters));
+  }, []);
+  
+    return (
+      <div className="d-flex flex-wrap justify-content-center">
+        {characters.map(character => (
+          <div className="card m-5" key={character.id} style={{ width: "18rem" }}>
+            <img src={character.image} alt={character.name} />
+            <div className="card-body">
+              <h5 className="card-title">{character.name}</h5>
+              <ul className="list-group list-group-light list-group-small">
+                <li className="list-group-item px-4 position-relative">
+                  Status:<b className="position-absolute badge bg-danger">{character.status}</b>
+                </li>
+              </ul>
+            </div>
+          </div>
+        ))}
       </div>
-      <ul className="list-group list-group-light list-group-small">
-        <li className="list-group-item px-4">Cras justo odio</li>
-        <li className="list-group-item px-4">Dapibus ac facilisis in</li>
-        <li className="list-group-item px-4">Vestibulum at eros</li>
-      </ul>
-      <div className="card-body">
-       
-      </div>
-    </div>
-  );
-};
+    );
+    
+}; 
+
+export default Cards;
+
+
+
